1c05dd77dd684bc083f9619760abc635a0d2c160
Kövesdi Ákos <kovesdiakos@ktch.hu>
2025-04-01 10:58:09 +0200

    Solution osztály javítás, hozzáadás

--- src/Solution.java
+++ src/Solution.java
@@ -4,15 +4,16 @@ public class Solution {
     static ArrayList<Car> carList = FileReader.readFile();
 
     public static void startSolution(){
-        sumSallary();
-        task02("BMW");
-        task03();
-        task04();
-        task05();
-        task06();
+        sumPrice(); //összegzés tétele
+        task02("BMW"); //megszámlálás tétele
+        task03("Citroen");
+        task04("Audi");
+        task05("BMW");
+        task06("Ford");
+        task07(); //szétválogatás tétel
     }
 
-    private static void sumSallary(){
+    private static void sumPrice(){
         // System.out.println(carList.get(0).getPlate());
         double sum = 0;
         for (Car car : carList) {
@@ -31,55 +32,71 @@ public class Solution {
         System.out.printf("%s számlaló: %d\n",brand, counter);
     }
 
-    private static void task03(){
+    private static void task03(String brand){
        int n = carList.size();
-       String ker = "Citroen";
        int i = 0;
-       while (i<n && !carList.get(i).getBrand().equals(ker)) {
+       while (i<n && !carList.get(i).getBrand().equals(brand)) {
         i++;
        }
        if (i<n) {
-        System.out.println("Van "+ ker);
+        System.out.println("Van "+ brand);
        } else {
-        System.out.println("Nincs "+ker);
+        System.out.println("Nincs "+brand);
        }
     }
 
-    private static void task04(){
-        String ker = "Audi";
+    private static void task04(String brand){
+        ;
         int i = 0;
-        while (!carList.get(i).getBrand().equals(ker)) {
+        while (!carList.get(i).getBrand().equals(brand)) {
          i++;
         }
          System.out.println("Hányadik helyen: "+ (i+1));
      }
 
-     private static void task05(){
+     private static void task05(String brand){
         int n = carList.size();
-        String ker = "BMW";
         int i = 0;
-        while (i<n && (!carList.get(i).getBrand().equals(ker))) {
+        while (i<n && (!carList.get(i).getBrand().equals(brand))) {
          i++;
         }
         if (i<n) {
-         System.out.println("Van "+ ker + ". Indexe: " + i);
+         System.out.println("Van "+ brand + ". Indexe: " + i);
         } else {
-         System.out.println("Nincs "+ker);
+         System.out.println("Nincs "+ brand);
         }
      }
 
-     private static void task06(){
-        ArrayList<Car> fordList = new ArrayList<>();
+     private static void task06(String brand){
+        ArrayList<Car> brandList = new ArrayList<>();
         for (Car car : carList) {
-            if (car.getBrand().equals("Ford")) {
-                fordList.add(car);
+            if (car.getBrand().equals(brand)) {
+                brandList.add(car);
             } 
         }
-        kiirLista(fordList);
+        kiirLista(brandList);
      }
      private static void kiirLista(ArrayList<Car> list){
         for (Car car : list) {
-            System.out.println(car.getPlate()+ " "+ car.getBrand());
+            System.out.printf("%s, %s, %.2f\n",car.getPlate(),car.getBrand(),car.getPrice());
         }
+     }
+
+     private static void task07(){
+        System.out.println("--------------\nSzétválogatás:");
+        System.out.println();
+        ArrayList<Car> expList = new ArrayList<>();
+        ArrayList<Car> cheapList = new ArrayList<>();
+        for (Car car : carList) {
+            if (car.getPrice()> 1100) {
+                expList.add(car);
+            }else{
+                cheapList.add(car);
+            }
+        }
+        System.out.println("Drága autók:");
+        kiirLista(expList);
+        System.out.println("Olcsó autók:");
+        kiirLista(cheapList);
      }
 }
